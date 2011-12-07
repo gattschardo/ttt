a9458b67db445934ecd0b670f31b731f0b19905c
Richard Molitor <gattschardo@gmail.com>
2011-12-07 20:27:06 +0100

    some more types, player switch

--- ttt.m
+++ ttt.m
@@ -5,85 +5,98 @@
 :- pred main(io::di, io::uo) is cc_multi.
 
 :- implementation.
-:- import_module int, string, list.
+:- import_module int, char, string, list.
 
-:- type ttt_board ---> board( x_stones  :: list(int),
-                              o_stones  :: list(int),
+:- type position ---> {int,int}.
+
+:- type player ---> 'X' ; 'O'.
+
+:- type ttt_board ---> board( x_stones  :: list(position),
+                              o_stones  :: list(position),
                               count     :: int ).
 
-:- type input ---> move(int) ; err ; exit.
+:- type input ---> move(position) ; err ; exit.
 
 :- func board_to_string(ttt_board) = string.
 board_to_string(board(Xs,Os,_)) = S :-
     SepLine = "-+-+-\n",
-    S = line(0,Xs,Os) ++ SepLine ++ line(3,Xs,Os) ++ SepLine ++ line(6,Xs,Os).
+    S = line(1,Xs,Os) ++ SepLine ++ line(2,Xs,Os) ++ SepLine ++ line(3,Xs,Os).
 
-:- func line(int,list(int),list(int)) = string.
-line(Offset,Xs,Os) = S :-
+:- func line(int,list(position),list(position)) = string.
+line(Line,Xs,Os) = S :-
     Sep = "|",
-    S = findchar(Offset+1,Xs,Os) ++ Sep ++ findchar(Offset+2,Xs,Os) ++ Sep ++ findchar(Offset+3,Xs,Os) ++ "\n".
+    S = findchar({Line,1},Xs,Os) ++ Sep
+     ++ findchar({Line,2},Xs,Os) ++ Sep
+     ++ findchar({Line,3},Xs,Os) ++ "\n".
 
-:- func findchar(int,list(int),list(int)) = string.
+:- func findchar(position,list(position),list(position)) = string.
 findchar(I,[X|Xs],Os) = (if X = I then "X" else findchar(I,Xs,Os)).
 findchar(I,[],[O|Os]) = (if O = I then "O" else findchar(I,[],Os)).
 findchar(_,[],[]) = " ".
 
-:- pred input(input::out, io::di, io::uo) is cc_multi.
-input(X,IOin,IOout) :-
-    io.write_string("X> ",IOin,IO1),
-    io.read_line_as_string(R,IO1,IOout),
+:- func ptc(player) = char.
+ptc('X') = 'X'.
+ptc('O') = 'O'.
+
+:- func toggle(player) = player.
+toggle('X') = 'O'.
+toggle('O') = 'X'.
+
+:- pred input(player::in, input::out, io::di, io::uo) is cc_multi.
+input(P,M,!IO) :-
+    io.format("%c> ",[c(ptc(P))],!IO),
+    io.read_line_as_string(R,!IO),
     (
         R = ok(S),
-        string.to_int(string.strip(S), Y),
-        Y > 0, Y < 10,
-        X = move(Y)%,
-        %IO2 = IOout
+        string.to_int(string.strip(S), I),
+        I > 0, I < 10,
+      	Y = 1 + (I - 1) mod 3, X = 1 + (I - 1) / 3,
+        M = move({X,Y})
     ;
         R = eof,
-        %IO2 = IOout,%io.write_string("\n",IO2,IOout),
-        X = exit
+        M = exit
     ;
-        %R = error(_),
-        %IO2 = IOout,%io.write_string("Error occured!\n",IO2,IOout),
-        X = err
-    %;
-        %io.write_string("enter number between 1 and 9\n",IO2,IO3),
-        %IO2 = IOout,
-        %X = err.
-        %input(X,IO3,IOout)
+        M = err
     ).
 
-:- pred game(ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
-game(Bi,Bo,!IO) :-
-    input(I,!IO),
+:- pred game(player::in, player::out, ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
+game(Pi,Po,Bi,Bo,!IO) :-
+    input(Pi,M,!IO),
     (
-      I = move(X),
-      Bn = Bi^x_stones := [X|Bi^x_stones],
-      output(Bn,Bo,!IO)
+      M = move(P),
+      (
+      	Pi = 'X',
+      	Bn = Bi^x_stones := [P|Bi^x_stones],
+      	output('O',Po,Bn,Bo,!IO)
+      ;
+      	Pi = 'O',
+      	Bn = Bi^o_stones := [P|Bi^o_stones],
+      	output('X',Po,Bn,Bo,!IO)
+      )
     ;
-      I = err,
-      err(Bi,Bo,!IO)
+      M = err,
+      err(Pi,Po,Bi,Bo,!IO)
     ;
-      I = exit,
-      quit(Bi,Bo,!IO)
+      M = exit,
+      quit(Pi,Po,Bi,Bo,!IO)
     ).
 
-:- pred output(ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
-output(Bi,Bo,!IO) :-
+:- pred output(player::in, player::out, ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
+output(!P,Bi,Bo,!IO) :-
   S = board_to_string(Bi),
   io.write_string(S ++ "\n",!IO),
-  game(Bi,Bo,!IO).
+  game(!P,Bi,Bo,!IO).
 
-:- pred err(ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
-err(!B,!IO) :-
+:- pred err(player::in, player::out, ttt_board::in, ttt_board::out, io::di, io::uo) is cc_multi.
+err(!P,!B,!IO) :-
     io.write_string("enter number between 1 and 9\n",!IO),
-    game(!B,!IO).
+    game(!P,!B,!IO).
 
-:- pred quit(ttt_board::in, ttt_board::out, io::di, io::uo) is det.
-quit(Bi,Bo,!IO) :-
+:- pred quit(player::in, player::out, ttt_board::in, ttt_board::out, io::di, io::uo) is det.
+quit(Pi,Po,Bi,Bo,!IO) :-
     io.write_string("\n",!IO),
-    Bo = Bi.
+    Po = Pi, Bo = Bi.
 
 main(!IO) :-
     B0 = board([],[],0),
-    output(B0,_,!IO).
+    output('X',_,B0,_,!IO).
